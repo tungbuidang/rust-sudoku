64a45de5cc6a586f3566ee83a818d620d8a5af57
sleepydragon <tungbui@structo3d.com>
2020-12-16 10:21:00 +0800

    add finding empty square function

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use rand::prelude::*;
+
 // struct SudokuNum {
 //     value: i8,
 //     display: bool
 // }
 // #[derive(Copy, Clone)]
+
 struct coordinate {
     x: usize,
     y: usize
@@ -20,12 +23,12 @@ impl Sudoku3x3 {
     //     coordinate{x, y}
     // }
 
-    fn set_num(&mut self, value: i8, x: usize, y : usize) {
-        self.matrix[y][x] = value;
+    fn set_num(&mut self, value: i8,coordinate: coordinate) {
+        self.matrix[coordinate.y][coordinate.x] = value;
     } 
 
-    fn get_num(&self, x: usize, y: usize) -> i8 {
-        self.matrix[y][x]
+    fn get_num(&self, coordinate: coordinate) -> i8 {
+        self.matrix[coordinate.y][coordinate.x]
     }
 
     fn print_sudoku(&self) {
@@ -64,17 +67,34 @@ impl Sudoku3x3 {
         }
         false
     }
-    fn valid_num(&self, num:i8, x:usize, y:usize) -> bool {
-        if self.same_num_in_column(num, x) {
+
+    fn valid_num(&self, num:i8, coordinate: coordinate) -> bool {
+        if self.same_num_in_column(num, coordinate.x) {
             return false
         }
-        if self.same_num_in_row(num, y) {
+        if self.same_num_in_row(num, coordinate.y) {
             return false
         }
-        if self.same_num_in_grid(num, x, y) {
+        if self.same_num_in_grid(num, coordinate.x, coordinate.y) {
             return false
         }
         return true
+    }
+
+    fn find_empty_square(&self) -> Option<coordinate> {
+        for row in 0..self.matrix.len() {
+            for col in 0..self.matrix.len() {
+                if self.matrix[row][col] == 0 {
+                    let coordinate = coordinate{x: col, y: row};
+                    return Some(coordinate)
+                }
+            }
+        }
+        None
+    }
+
+    fn generate_full_board(&self) {
+        
     }
 }
 
@@ -83,9 +103,22 @@ fn main() {
         matrix: [[0;9];9],
         hint: 3
     };
-    my_sudoku.set_num(3, 8, 8);
+    let your_sudoku = Sudoku3x3 {
+        matrix: [[1;9];9],
+        hint: 3
+    };
+
+    my_sudoku.set_num(3, coordinate{x:0,y:0});
     my_sudoku.print_sudoku();
     println!("{}", my_sudoku.same_num_in_column(3,8));
     println!("{}", my_sudoku.same_num_in_row(3,7));
     println!("{}", my_sudoku.same_num_in_grid(3,7,6));
+    let result = my_sudoku.find_empty_square();
+    match result {
+        Some(coordinate) => {
+            println!("there's at least one empty square");
+            println!("{}, {}",coordinate.x, coordinate.y);
+        },
+        None => println!("there's no empty square"),
+    }
 }
